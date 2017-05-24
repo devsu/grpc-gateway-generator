ca4c620571af1cc30518f1dfef9cb549effb144c
Cesar Salazar <csalazar@devsu.com>
2017-05-24 09:31:44 -0500

    allow to configre basePath per service

--- README.md
+++ README.md
@@ -31,52 +31,66 @@ git clone https://github.com/devsu/grpc-gateway-generator
 ## Usage
 
 ```bash
-./generate.sh protos-path config-file destination-path  
+./generate.sh protos-path config-file project-path  
 ```
+
+### Arguments
+
+- `protos-path`: The path to the protos folder. 
+
+  The package names of the protos files must correspond to the folder structure. For example:
+   
+  - `/protos/myapp/my-file.proto` must have the package `myapp`, and 
+  - `/protos/myapp/mymicroservice/another-file.proto` must have the package `myapp.mymicroservice`
+
+- `config-file`: Path to a `config.json` file. The config file has two sections: `gateway` and `backends`. 
+    
+  In the gateway section, you define the address that the gateway will listen into.
+    
+  The backends is an array of objects with 3 properties: 
+    
+  - `package`: name of the package.
+  - `backend`: what addres the GRPC server is running.
+  - `services`: An object with the services names as keys, and the base paths of the services as values. 
+  
+  See [config.json](https://github.com/devsu/grpc-gateway-generator/blob/master/example/config.json) for an example of how it should look:
+    
+   ```json
+  {
+    "gateway": {
+      "listen": ":9090"
+    },
+    "backends": [
+      {
+        "package": "myapp",
+        "backend": "localhost:3000",
+        "services": {
+          "GreeterService": "/sample/greeter/"
+        }
+      },
+      {
+        "package": "myapp.mysubapp",
+        "backend": "localhost:3000",
+        "services": {
+          "AnotherGreeterService": "/sample/another-greeter/"
+        }
+      }
+    ]
+  }
+  ```
+
+- `project-path`: The path of the go project to be created. The gateway will be created in `${GOPATH}/src/${projectPath}`. Read [How to write Go Code](https://golang.org/doc/code.html) to learn how to choose a proper project path. 
 
 ## Step by step guide
 
 - First, create your protos and add the corresponding annotations. You can see the [grpc-gateway documentation](https://github.com/grpc-ecosystem/grpc-gateway#usage) or the sample file in [examples/protos/myapp](https://github.com/devsu/grpc-gateway-generator/blob/master/example/protos/myapp/sample.proto) for an example.
-- Then you need to create a config file. 
-
-  The config file has two sections: `gateway` and `backends`. 
-  
-  In the gateway section, you define the address that the gateway will listen into.
-  
-  The backends is an array of objects with 3 properties: 
-  
-    - `package`: name of the package.
-    - `backend`: what addres the GRPC server is running.
-    - `basePath`: what's the base url for the services on this proto file.
-
-  See [config.json](https://github.com/devsu/grpc-gateway-generator/blob/master/example/config.json) for an example of how it should look:
-  
-  ```json
-    {
-      "gateway": {
-        "listen": ":9090"
-      },
-      "backends": [
-        {
-          "package": "myapp",
-          "backend": "localhost:3000",
-          "basePath": "/sample/greeter/"
-        },
-        {
-          "package": "myapp.mysubapp",
-          "backend": "localhost:3000",
-          "basePath": "/sample/another-greeter/"
-        }
-      ]
-    }
-  ```
-  
+- Then you need to create a config file. (See instructions above)
 - Then you just need to run the command with the right arguments.
 
   For example, to create a grpc-gateway using the information in the example:
 
   ```bash
-  ./generate.sh example/protos example/config.json example/generated 
+  ./generate.sh example/protos example/config.json github.com/my-user/my-gateway
   ```
 
 ## TODO
--- generate.sh
+++ generate.sh
@@ -6,13 +6,14 @@ fi
 
 PROTOS_PATH=${1}
 CONFIG_FILE=${2}
-DESTINATION_PATH=${3}
+PROJECT_PATH=${3}
+DESTINATION_PATH=${GOPATH}/src/${PROJECT_PATH}
 TEMPLATE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/template"
 
 OS=$(uname -s)
 SED_OPTS="-i"
 if [ $OS = "Darwin" ]; then
-  SED_OPTS="-i ''"
+  SED_OPTS="-i .bak"
 fi
 
 echo "GENERATING GATEWAY:"
@@ -76,37 +77,67 @@ mv ${DESTINATION_PATH}/http.proto ${PROTOS_PATH}/google/api/
 echo "GENERATING... swagger list of files"
 echo "[" >> ${DEFINITIONS_PATH}/list.json
 find ${DEFINITIONS_PATH} -type f -name "*.json" -exec echo "\"{}\"," >> ${DEFINITIONS_PATH}/list.json \;
-sed $SED_OPTS -e "s|${DESTINATION_PATH}||g" \
+sed ${SED_OPTS} -e "s|${DESTINATION_PATH}||g" \
   -e '/list.json",/d' \
+  -e '/google\/protobuf/d' \
   -e '$s/,$//g' \
   "${DEFINITIONS_PATH}/list.json"
 echo "]" >> ${DEFINITIONS_PATH}/list.json
 
 echo "MODIFYING... swagger files, to include authorization bearer"
 SECURITY_DEFINITIONS='"securityDefinitions":{"Bearer":{"type":"apiKey","name":"Authorization","in":"header"}}';
-find ${DEFINITIONS_PATH} -type f -name "*.json" -exec sed $SED_OPTS -e "\$s/}/,${SECURITY_DEFINITIONS}}/" {} \;
+find ${DEFINITIONS_PATH} -type f -name "*.json" -exec sed ${SED_OPTS} -e "\$s/}/,${SECURITY_DEFINITIONS}}/" {} \;
 
 echo "MODIFYING... grpc-gateway.go"
-FOLDERS=$(cat ${CONFIG_FILE} | jq '.backends | .[] | .package' | sed -e 's|"||g' -e 's|\.|/|g')
-BACKENDS=($(cat ${CONFIG_FILE} | jq '.backends | .[] | .backend'))
-BASE_PATHS=($(cat ${CONFIG_FILE} | jq '.backends | .[] | .basePath'))
-COUNT=0;
-for FOLDER in ${FOLDERS}
+PACKAGES=$(jq '.backends | .[] | .package' <${CONFIG_FILE} | sed -e 's|"||g')
+COUNT=0
+for PACKAGE in ${PACKAGES}
 do
-  sed $SED_OPTS "s|.*ADD IMPORTS HERE.*|  gw${COUNT} \"./stubs/${FOLDER}\""'\
-&|' "${DESTINATION_PATH}/grpc-gateway.go"
+  FOLDER=$(sed -e 's|\.|/|g' <<< ${PACKAGE})
+  BACKEND_OBJECT=$(jq '.backends | .[] | select(.package=="'${PACKAGE}'")' <${CONFIG_FILE})
+  SERVICES_OBJECT=$(jq '.services' <<< ${BACKEND_OBJECT})
+  BACKEND=$(jq '.backend' <<< ${BACKEND_OBJECT})
 
-  METHODS=$(find ${STUBS_PATH}/${FOLDER} -type f -maxdepth 1 -print | xargs grep -E "Register(.*)FromEndpoint" -ho | uniq)
+  echo " -> Processing ${PACKAGE}"
 
-  for ENDPOINT_METHOD in ${METHODS}
-  do
-    PARAMETERS="${BACKENDS[COUNT]}, ${BASE_PATHS[COUNT]}, gw${COUNT}.${ENDPOINT_METHOD}"
-    sed $SED_OPTS -e "s|.*ADD ENDPOINTS HERE.*|  loadEndpoint(ctx, serverMux, ${PARAMETERS})"'\
+  if [ "${SERVICES_OBJECT}" == "null" ]; then
+    echo "    No services defined for ${PACKAGE}"
+  else
+    SERVICES=$(jq 'keys[]' <<< ${SERVICES_OBJECT})
+    BASE_PATHS=($(jq '.[]' <<< ${SERVICES_OBJECT}))
+    SERVICES_ARRAY=(${SERVICES})
+    SERVICES_LENGTH=${#SERVICES_ARRAY[@]}
+    if [ ${SERVICES_LENGTH} -eq 0 ]; then
+      echo "    No services defined for ${PACKAGE}"
+    else
+      sed ${SED_OPTS} "s|.*ADD IMPORTS HERE.*|  gw${COUNT} \"${PROJECT_PATH}/stubs/${FOLDER}\""'\
 &|' "${DESTINATION_PATH}/grpc-gateway.go"
-  done
+    fi
+
+    COUNT2=0
+    for SERVICE in ${SERVICES}
+    do
+      ENDPOINT="Register"$(sed -e 's|"||g' <<< ${SERVICE})"HandlerFromEndpoint"
+      echo "    Adding ${SERVICE}"
+      PARAMETERS="${BACKEND}, ${BASE_PATHS[COUNT2]}, gw${COUNT}.${ENDPOINT}"
+      sed ${SED_OPTS} -e "s|.*ADD ENDPOINTS HERE.*|  loadEndpoint(ctx, serverMux, ${PARAMETERS})"'\
+&|' "${DESTINATION_PATH}/grpc-gateway.go"
+      COUNT2=$((COUNT2+1))
+    done
+  fi
+
   COUNT=$((COUNT+1))
 done
 
 echo "CONFIGURING... grpc-gateway.go"
 LISTEN=$(cat ${CONFIG_FILE} | jq '.gateway | .listen')
-sed $SED_OPTS -e "s|\"LISTEN\"|${LISTEN}|" "${DESTINATION_PATH}/grpc-gateway.go"
+sed ${SED_OPTS} -e "s|\"LISTEN\"|${LISTEN}|" "${DESTINATION_PATH}/grpc-gateway.go"
+
+echo "FIXING PATHS... in auto generated go files"
+PACKAGES=$(find ${PROTOS_PATH} -type f -name "*.proto" -print | xargs grep -E "package( )+[^=\"]*;" -ho | uniq | sed 's/;/ /g')
+for PACKAGE in ${PACKAGES}
+do
+  if [[ $PACKAGE != package* ]] && [[ $PACKAGE != google* ]] ; then
+    find ${STUBS_PATH} -type f -name "*.go" -print0 | xargs -0 sed ${SED_OPTS} -e "s|\"${PACKAGE}\"|\"${PROJECT_PATH}/stubs/${PACKAGE}\"|g"
+  fi
+done
